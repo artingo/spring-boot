0778c901459002b529c88bb6a0eb044f2c6064ab
Alfred Walther-Weyland <alfred.walther@artingo.net>
2020-06-03 21:34:37 +0200

    Initial commit

---
+++ src/main/java/de/karrieretutor/springboot/SimpleController.java
@@ -0,0 +1,28 @@
+package de.karrieretutor.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "index";
+    }
+
+    @GetMapping("/{name}.html")
+    public String htmlMapping(@PathVariable(name = "name") String name) {
+        System.out.println("htmlMapping: " + name);
+        return name;
+    }
+}
