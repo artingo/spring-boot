b73f319e3f13c5295dad1510bd713acb7859f463
Alfred Walther-Weyland <alfred.walther@artingo.net>
2020-06-04 11:27:37 +0200

    Added dynamic Title

--- src/main/java/de/karrieretutor/springboot/SimpleController.java
+++ src/main/java/de/karrieretutor/springboot/SimpleController.java
@@ -3,11 +3,9 @@ package de.karrieretutor.springboot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class SimpleController {
@@ -17,12 +15,13 @@ public class SimpleController {
     @GetMapping("/")
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
+        model.addAttribute("titel", "Demo Web App");
         return "index";
     }
 
     @GetMapping("/{name}.html")
-    public String htmlMapping(@PathVariable(name = "name") String name) {
-        System.out.println("htmlMapping: " + name);
+    public String htmlMapping(@PathVariable(name = "name") String name, Model model) {
+        model.addAttribute("titel", StringUtils.capitalize(name));
         return name;
     }
 }
